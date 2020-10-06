a5be0e501f9cc77f9234bf5e717c692dfef7670a
saikat2007 <67913259+saikat2007@users.noreply.github.com>
2020-10-06 12:28:10 +0530

    Add files via upload

---
+++ car.js
@@ -0,0 +1,24 @@
+class car {
+    var car,speed,weight
+    car=createSprite(50,200,50,50);
+    
+speed=random(55,90);
+weight=random(400,1500);
+if(wall.x car.x < (car.width+wall.widthg)/2)
+{
+    car.velocityX=0;
+    var deformation=0.5*weight*speed* speed/22500;
+    if(deformation>180)
+    {
+    car.shapeColor=color(255,0,0);
+    }
+    if(deformation<180 && deformation>100)
+    {
+        car.shapeColor=color(230,230,0);
+    }
+    if(eformation<100)
+    {
+        car.shapeColor=color(o,255,0):
+    }
+}
+}
---
+++ wall.js
@@ -0,0 +1,10 @@
+class wall{
+    var wall
+    car=createSprite(1500,200,60,height/2);
+   
+        display(){
+          var pos =this.body.position;
+        
+          fill("80,80,80");
+        }
+    }
